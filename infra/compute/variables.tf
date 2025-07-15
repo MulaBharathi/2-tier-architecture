20d1d1ce7a4f2a649d6c3413100a78e5051ecfd6
Bharathimula <bharathim273@gmail.com>
2025-07-15 08:09:41 +0530

    Intial commit

---
+++ infra/compute/variables.tf
@@ -0,0 +1,25 @@
+variable "key_name" {
+  description = "EC2 key pair name for SSH"
+  type        = string
+}
+
+variable "public_key_path" {
+  description = "Path to the public key file"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID to launch EC2 in"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "Public subnet for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
