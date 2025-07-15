82ba800da3f860331a94b600cc44b2b707267d88
Bharathimula <bharathim273@gmail.com>
2025-07-15 11:06:12 +0530

    Intial commit

--- infra/compute/security.tf
+++ infra/compute/security.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-sg"
   description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.main_vpc.id
+  vpc_id      = var.vpc_id
 
   ingress {
     from_port   = 22
