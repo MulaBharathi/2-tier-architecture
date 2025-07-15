2fde8c6bc8a9aaf5b9d83eeb41e37fbab5990a98
Bharathimula <bharathim273@gmail.com>
2025-07-15 11:23:08 +0530

    Modified

--- infra/compute/ec2.tf
+++ infra/compute/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.public_subnet.id
+  subnet_id              = var.public_subnet.id
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
