74bfe5eb764884524146acf50b1fe078119596a9
Bharathimula <bharathim273@gmail.com>
2025-07-15 11:28:45 +0530

    Modified

--- infra/networking/outputs.tf
+++ infra/networking/outputs.tf
@@ -1,10 +1,12 @@
 output "vpc_id" {
-  description = "VPC ID created in networking module"
-  value       = aws_vpc.main.id
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
 }
 
 output "private_subnet_id" {
-  description = "Public Subnet ID to launch EC2 instance"
-  value       = aws_subnet.private[0].id
+  value = aws_subnet.private_subnet.id
 }
 
