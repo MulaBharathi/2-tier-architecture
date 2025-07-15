1cbc6d45f09748776b5d1ec37590ad58837a827b
Bharathimula <bharathim273@gmail.com>
2025-07-15 10:57:44 +0530

    Cleanup unused main.tf files and update networking main.tf comment

--- infra/compute/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0f918f7e67a3323f0"
-  instance_type = var.instance_type
-  ...
-}
-
--- infra/networking/main.tf
+++ infra/networking/main.tf
@@ -1,13 +1,3 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  ...
-}
-
-resource "aws_subnet" "public" {
-  count                   = 1
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  ...
-}
+# This main.tf is intentionally left empty.
+# Resources are split into vpc.tf, subnets.tf, igw.tf, and outputs.tf
 
