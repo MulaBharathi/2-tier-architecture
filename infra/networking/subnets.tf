8f7fe03ed6d5fb322e129140a047e7fc7ee5d0b4
Bharathimula <bharathim273@gmail.com>
2025-07-15 12:04:32 +0530

    Modified

--- infra/networking/subnets.tf
+++ infra/networking/subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
+  vpc_id                  = aws_vpc.maiin.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
