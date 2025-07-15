5412f8606f2cebb4fad1aab593392940c8e96058
Bharathimula <bharathim273@gmail.com>
2025-07-15 11:52:50 +0530

    Modified

--- infra/networking/subnets.tf
+++ infra/networking/subnets.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.main_vpc.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-south-1a"
 
