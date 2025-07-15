45e2172fc1e427571553bfbdb41e18014cbca7d4
Bharathimula <bharathim273@gmail.com>
2025-07-15 16:10:24 +0530

    Intial commit

--- infra/database/variables.tf
+++ infra/database/variables.tf
@@ -20,7 +20,7 @@ variable "vpc_id" {
 }
 
 variable "private_subnet_id" {
-  description = "list of Private subnet ID for subnet group"
+  description = "list of Private subnet IDs to use for the RDS subnet group"
   type        = list(string)
 }
 
