40b7282d0e23ed6cc810655f750a7a13692a4203
Bharathimula <bharathim273@gmail.com>
2025-07-15 11:50:13 +0530

    modified igw

--- infra/networking/igw.tf
+++ infra/networking/igw.tf
@@ -7,7 +7,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
