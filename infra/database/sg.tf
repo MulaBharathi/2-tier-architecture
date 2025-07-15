8259900c9b93c63ea339ec8956358a9e8300b726
Bharathimula <bharathim273@gmail.com>
2025-07-15 11:56:36 +0530

    modofied

---
+++ infra/database/sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow MySQL access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Change this in production for tighter security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
+}
+
